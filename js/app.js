66ed8d031eed22fc03fd3a1368c1602798ad51d4
sheenashahangian <sheenas@bu.edu>
2021-11-21 11:08:18 -0700

    logic for travel form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 
-function findTravelLocation(userResponses) {
+function findTravelLocation() {
+
+    // get the user responses from the form
+    let desertOrMountainResponse = document.getElementById("desertOrMountain");
+    let desertOrMountainText = desertOrMountainResponse.options[desertOrMountainResponse.selectedIndex].text;
+
+    let climateResponse = document.getElementById("humidOrArid");
+    let climateResponseText = climateResponse.options[climateResponse.selectedIndex].text;
+
+    let townProximityResponse = document.getElementById("nearTownOrNo");
+    let townProximityText = townProximityResponse.options[townProximityResponse.selectedIndex].text;
+
     // the possible locations that could be recommended to the user
     let locationList = ["Snowy Range Mountains", "North Cascades of Washington", "Leadville, Colorado", "Great Sand Dunes National Park"];
 
@@ -11,20 +22,20 @@ function findTravelLocation(userResponses) {
 
     // First question asks the user if they prefer desert landscapes or mountain landscapes.
     // If the user responded that they prefer the desert, immediately recommend sand dunes
-    if (userResponses[0] === "desert") {
+    if (desertOrMountainText === "desert") {
         chosenLocation = locationList[3];
     }
 
     // Second question asks the user if they prefer humid or arid climates.
     // If they prefer humid, how recommend North Cascades, as it's the only mountain location 
     // on the list with a humid climate.
-    else if (userResponses[1] == "humid") {
+    else if (climateResponseText == "humid") {
         chosenLocation = locationList[1];
     }
 
     // Third question asks the user if they want to be near a town with things to do.
     // If they do, Leadville will be recommended, as it is a mountain town.
-    else if (userResponses[2] == "yes") {
+    else if (townProximityText == "yes") {
         chosenLocation = locationList[2];
     }
 
