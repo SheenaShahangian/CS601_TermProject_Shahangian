56e3b9c0a474fecc6a28234b22ad416c2bbb1818
sheenashahangian <sheenas@bu.edu>
2021-11-21 08:46:18 -0700

    created logic for travel quiz

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+function findTravelLocation(userResponses) {
+    // the possible locations that could be recommended to the user
+    let locationList = ["Snowy Range Mountains", "North Cascades of Washington", "Leadville, Colorado", "Great Sand Dunes National Park"];
+
+    // the location to be recommended to the user
+    let chosenLocation = "";
+
+    // this will be added to the beginning of the result
+    let resultIntro = "<h3>You Should Travel To: </h3>";
+
+    // First question asks the user if they prefer desert landscapes or mountain landscapes.
+    // If the user responded that they prefer the desert, immediately recommend sand dunes
+    if (userResponses[0] === "desert") {
+        chosenLocation = locationList[3];
+    }
+
+    // Second question asks the user if they prefer humid or arid climates.
+    // If they prefer humid, how recommend North Cascades, as it's the only mountain location 
+    // on the list with a humid climate.
+    else if (userResponses[1] == "humid") {
+        chosenLocation = locationList[1];
+    }
+
+    // Third question asks the user if they want to be near a town with things to do.
+    // If they do, Leadville will be recommended, as it is a mountain town.
+    else if (userResponses[2] == "yes") {
+        chosenLocation = locationList[2];
+    }
+
+    // Otherwise recommend Snowy Range Mountains, as there isn't much nearby.
+    else {
+    }
+
+    // Write the results to the page
+    document.getElementById("quizResult").innerHTML = resultIntro + "<p>" + chosenLocation + "</p>";
+}
--- js/buttons.js
+++
@@ -1,8 +0,0 @@
-
-function openButtonNewTab(url) {
-    parent.open(url);
-}
-
-function openButtonCurrentTab(url) {
-    window.open(url);
-}
