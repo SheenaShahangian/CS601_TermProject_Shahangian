0d42324aee3e7229c50f955f4597460a7dd18b3e
sheenashahangian <sheenas@bu.edu>
2021-11-29 22:23:01 -0700

    update travel quiz logic

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ function findTravelLocation() {
     let townProximityText = townProximityResponse.options[townProximityResponse.selectedIndex].text;
 
     // the possible locations that could be recommended to the user
-    let locationList = ["Snowy Range Mountains", "North Cascades of Washington", "Leadville, Colorado", "Great Sand Dunes National Park"];
+    let locationList = ["Snowy Range Mountains", "North Cascades of Washington", "Leadville, Colorado", "Great Sand Dunes National Park", 
+    "Great Smoky Mountains National Park", "Moab, Utah"];
 
     // the location to be recommended to the user
     let chosenLocation = "";
@@ -23,21 +24,41 @@ function findTravelLocation() {
     // this will be added to the beginning of the result
     let resultIntro = "<h3>You Should Travel To: </h3>";
 
-    // First question asks the user if they prefer desert landscapes or mountain landscapes.
-    // If the user responded that they prefer the desert, immediately recommend sand dunes
+    // The first question asks if the user prefers desert or mountains. As there are only 
+    // two desert locations on the list, we can cancel out the rest.
+    // If the user prefers a desert landscape and an arid climate, recommend Moab.
+    // If the user prefers a desert landscape, and doesn't prefer arid, recommend Great 
+    // Sand Dunes, as it's less arid than Moab
     if (desertOrMountainText === "desert") {
-        chosenLocation = locationList[3];
+        // if the person also prefers arid climates, recommend Moab
+        if (climateResponseText ==="arid") {
+            chosenLocation = locationList[5];
+        }
+        // otherwise recommend Great Sand Dunes, as it gets more humidity
+        else {
+            chosenLocation = locationList[3];
+        }
     }
 
     // Second question asks the user if they prefer humid or arid climates.
-    // If they prefer humid, how recommend North Cascades, as it's the only mountain location 
-    // on the list with a humid climate.
+    // There are two mountain options with humid climates: Great Smoky Mountains 
+    // and  North Cascades, so we must narrow it down
     else if (climateResponseText == "humid") {
-        chosenLocation = locationList[1];
+        // check to see if the person said they wanted to be close to a town, and 
+        // if so, recommend Great Smoky Mountains
+        if (townProximityText === "yes") {
+            chosenLocation = locationList[4];
+        }
+        // otherwise, recommend North Cascades
+        else {
+            chosenLocation = locationList[1];
+        }
     }
 
     // Third question asks the user if they want to be near a town with things to do.
-    // If they do, Leadville will be recommended, as it is a mountain town.
+    // At this point, we're only recommending arid climate locations, since we singled 
+    // out all humid climates in the block above.
+    // If they do want to be near a town, Leadville will be recommended, as it is a mountain town.
     else if (townProximityText == "yes") {
         chosenLocation = locationList[2];
     }
