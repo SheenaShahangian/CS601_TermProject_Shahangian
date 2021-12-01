e548267a34b56a800dc466f3fbc73847b523387d
sheenashahangian <sheenas@bu.edu>
2021-11-30 17:13:18 -0700

    work on accordion dropdown

--- js/app.js
+++ js/app.js
@@ -74,7 +74,28 @@ function findTravelLocation() {
 
 
 
-/* ---------------------------------- LOGIC FOR TRAVEL BUCKET LIST ---------------------------------- */
+/* ---------------------------------- LOGIC FOR Q&A DROPDOWN ---------------------------------- */
+
+// flag that indicates whether the question is clicked on, default to false
+let toggleFlag = false;
+
+function toggleDropdownAccordion(question) {
+
+    const answer = question.nextElementSibling;
+
+    // if the toggle flag is false, show the contents when clicked on
+    // get the question's answer
+    if (toggleFlag === false) {
+        answer.className = "visible";
+        toggleFlag = true; // change toggle flag
+    }
+    // if the toggle flag is true, hide the contents when the user clicks 
+    // on the question
+    else {
+        answer.className = "invisible";
+        toggleFlag = false;
+    }
+}
 
 
 
