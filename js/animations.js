271858186ac8129c69e2d973e5e9ffb18e933b38
sheenashahangian <sheenas@bu.edu>
2021-11-30 21:54:39 -0700

    work on text animations

---
+++ js/animations.js
@@ -0,0 +1,51 @@
+// get the element that requires animation (of class .animated_heading)
+const animatedHeading = document.querySelector(".animated_heading");
+
+// get the text of that element and split it to apply animations per letter
+const animatedHeadingContents = animatedHeading.innerText.split("");
+
+// set the original text content to be nothing so we don't have a duplicate heading
+animatedHeading.innerText = "";
+
+// get the current character/letter index
+let currentLetterIndex = 0;
+
+
+
+/* ------------------ LOGIC TO MAKE EACH LETTER A SPAN ------------------- */
+
+// start at index 0 (to be used in the for...of loop)
+let index = 0;
+// iterate over each letter and make each one into a span so we can animate
+for (letter of animatedHeadingContents) {
+    animatedHeading.innerHTML += "<span class='letter_span'>" + animatedHeadingContents[index] + "</span>";
+    index++; // increment index, as for...of loop doesn't keep tabs
+}
+
+/* ------------------------------------------------------------------------ */
+
+// the time between animations will be 80 ms
+let animationIntervalTimer = setInterval(makeAnimation, 80);
+
+/* This function is actually responsible for making the animation */
+function makeAnimation() {
+    // we need to get all the letters (that are contained in the spans of class .letter_span)
+    const letterSpans = animatedHeading.querySelectorAll(".letter_span")[currentLetterIndex];
+
+    // we need to change the styles, so add class of fade so those styles can be applied
+    letterSpans.classList.add("fading_animation");
+    currentLetterIndex++;
+
+    // we want to stop after going through all letters, so it's the length of the heading
+    const stoppingPoint = animatedHeadingContents.length;
+
+    // stop once we get to the end of all the characters/letters in the heading,
+    // then we want to stop
+    if (currentLetterIndex === stoppingPoint) {
+
+        // clears the timer we set up earlier
+        clearInterval(animationIntervalTimer);
+        animationIntervalTimer = null;
+        return;
+    }
+}
