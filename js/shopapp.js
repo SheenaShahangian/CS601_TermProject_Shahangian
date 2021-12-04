9acb046527118bb30271fc02b0282770cd92d29d
sheenashahangian <sheenas@bu.edu>
2021-12-04 14:45:08 -0700

    vue on shop page

---
+++ js/shopapp.js
@@ -0,0 +1,54 @@
+let shopapp = new Vue({
+    el: '#shopapp',
+    data: {
+        printShopName: "The Prints", // name of the print shop
+
+        printShopIntro: `Below, you'll find a curated collection of my favorite landscape prints. Each of them can 
+        be purchased in a high quality walnut wood frame. These are heirloom quality prints that can 
+        be passed down from generation to generation. My intent is that these images inspire you to get 
+        out and explore the great outdoors yourself. Each of these places brought me an excitement and 
+        contentment that only nature can.`,
+
+        printPrice: "Price: $247.95", // price is the same for all prints
+
+        // data for print one
+        printOneName: "Half Dome",
+        printOneImageFilename: "multimedia/framed-art-for-sale-Half-Dome.jpg",
+        printOneUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef2b2311cc7021a907c171e",
+
+        // data for print two
+        printTwoName: "Yosemite",
+        printTwoImageFilename: "multimedia/framed-art-for-sale-Yosemite.jpg",
+        printTwoUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef382f9a687ba0d800aa8ca",
+
+        // data for print three
+        printThreeName: "Moab",
+        printThreeImageFilename: "multimedia/framed-art-for-sale-Moab.jpg",
+        printThreeUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef2a71e1cc7020e9c60e67d",
+
+        // data for print four
+        printFourName: "Rainier",
+        printFourImageFilename: "multimedia/framed-art-for-sale-Mt-Rainier.jpg",
+        printFourUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef2b2e91cc7021a907c1756",
+
+        // data for print five
+        printFiveName: "Utah Desert",
+        printFiveImageFilename: "multimedia/framed-art-for-sale-Capitol-Reef.jpg",
+        printFiveUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef2a9521cc7021a907c14dc",
+
+        // data for print six
+        printSixName: "Snowy Alpine",
+        printSixImageFilename: "multimedia/framed-art-for-sale-snowy-mountains.jpg",
+        printSixUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef37c9237dfb2110c9a2667",
+
+        // data for print seven
+        printSevenName: "Aqua Lake",
+        printSevenImageFilename: "multimedia/framed-art-for-sale-Rocky-Mountain-lake.jpg",
+        printSevenUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef2a6b41cc70217a89e83cb",
+
+        // data for print eight
+        printEightName: "Telluride",
+        printEightImageFilename: "multimedia/framed-art-for-sale-Telluride.jpg",
+        printEightUrl: "https://sheenashahangian.pic-time.com/art/adventurelandscapes/5ef3826fa687ba050c08db9d"
+    },
+});
