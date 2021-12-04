624d8addd038854dae4942afb55d0372658ef9fb
sheenashahangian <sheenas@bu.edu>
2021-12-04 11:29:29 -0700

    complete fetch

--- js/bucketlist.js
+++ js/bucketlist.js
@@ -1,20 +1,19 @@
-// get the travel bucket list container (which is a unordered list)
+// get the travel bucket list container (which is a unordered list we need to populate)
 bucketListContainer = document.querySelector(".travel_list");
 
-// url for the JSON file
+// url for the JSON file containing bucket list data
 let locationsUrl = "https://vigilant-allen-c8453f.netlify.app/js/bucketlist.json";
 
 async function fetchBucketList() {
     // fetch the data in the bucket list JSON file
     await fetch(locationsUrl).then(
         (response) => {
-            // log the status
-            console.log(`Status ${response.status}`);
-
+            // if response status is ok, proceed
             if (response.status === 200) {
                 // return the JSON object
                 return response.json();
             }
+            // otherwise throw an error
             else {
                 throw "Apologies, but we could't get your travel data!";
             }
@@ -25,9 +24,9 @@ async function fetchBucketList() {
             // this will hold the list contents
             let locationList = "";
 
+            // iterate over the full number of locations in the JSON file
             for (let i = 0; i < data.bucket_list_locations.length; i++) {
-                console.log("in loop");
-                console.log(data.bucket_list_locations[i].location.name);
+                // add each location as an li element
                 locationList += `<li>${data.bucket_list_locations[i].location.name}</li>`;
             }
 
@@ -35,10 +34,12 @@ async function fetchBucketList() {
             bucketListContainer.innerHTML = locationList;
         }
     )
-    // error message if there's an issue fetching data
+    // error message if an issue arises when fetching the bucket list data
     .catch(error => {
         console.log("Dang it! We had an issue fetching the location info!");
     });
 }
 
+// call the fetch functionality upon js loading, as we want to have this list populated 
+// as soon as possible on the page
 fetchBucketList();
