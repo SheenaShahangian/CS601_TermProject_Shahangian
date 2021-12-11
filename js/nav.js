a072d00171e97f84e573ae4a4573572a8e58316b
sheenashahangian <sheenas@bu.edu>
2021-12-10 23:01:27 -0700

    complete  hamburger navigation

--- js/nav.js
+++ js/nav.js
@@ -4,25 +4,11 @@ on the hamburger menu. */
 // get the navigation links
 let navLinks = document.querySelectorAll(".nav_links");
 
-// set a boolean to determine if the hamburger has been clicked, default to false
-let hamburgerClicked = false;
-
 /* This is a function that allows shows and hides the navigation links, 
 when the user clicks on the hamburger menu icon. */
 function triggerMenuView() {
 
-    // if the hamburgerClicked bool is false, show the menu links
-    if (hamburgerClicked === false) {
-        for (link of navLinks) {
-            link.style.display = "block";
-        }
-        hamburgerClicked = true; // change flag
-    }
-    // if the hamburgerClicked bool is true, hide the menu links
-    else {
-        for (link of navLinks) {
-            link.style.display = "none";
-        }
-        hamburgerClicked = false; // change flag
+    for (link of navLinks) {
+        link.classList.toggle('view');
     }
 }
